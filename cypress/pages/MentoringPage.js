cf11648bbb222f76665e4eab8d3e63a8e6beb539
rahmansopiann <rahmansopian6@gmail.com>
2025-07-21 11:09:01 +0700

    Maintain button element "Ajukan Sesi Mentoring" to "Ajukan Jadwal"

--- cypress/pages/MentoringPage.js
+++ cypress/pages/MentoringPage.js
@@ -27,7 +27,7 @@ class MentoringPage {
   }
 
   attemptToJoinProgram() {
-    cy.contains("Ajukan Sesi Mentoring").should('be.visible').and('be.enabled').click();
+    cy.contains("Ajukan Jadwal").should('be.visible').and('be.enabled').click();
   }
 }
 
